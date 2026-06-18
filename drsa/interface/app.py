44ab7ebbc0322ceb01781e8548e5c3764a1e5eac
silvanozap <silvano.zappala@phd.unict.it>
2026-06-18 19:44:02 +0200

    fix roman

--- drsa/interface/app.py
+++ drsa/interface/app.py
@@ -442,7 +442,7 @@ def bibtex():
 }
 @article{greco2001,
   title     = {Rough sets theory for multicriteria decision analysis},
-  author    = {Greco, Salvatore and Matarazzo, Benedetto and S{\l}owi{\'n}ski, Roman},
+  author    = {Greco, Salvatore and Matarazzo, Benedetto and S{\\l}owi{\\'n}ski, Roman},
   journal   = {European journal of operational research},
   volume    = {129},
   number    = {1},
